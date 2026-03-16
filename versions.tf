e2dea108f3fb6877695171062ad6d570e31a980c
Ed McLain <emclain@nibuild.com>
2026-03-16 09:48:33 -0500

    Working on version upgrades

--- versions.tf
+++ versions.tf
@@ -2,9 +2,6 @@ terraform {
   required_version = "~> 1.1.7"
 
   required_providers {
-    aws      = "~> 2.0"
-    template = "~> 2.0"
-    local    = "~> 1.2"
-    null     = "~> 2.0"
+    aws = ">= 3.0"
   }
 }
